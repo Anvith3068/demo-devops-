4c327df6fb057997a52f62e0a50f7d158d2cba02
Anvith G <anvithgowda2219@gmail.com>
2026-04-15 17:24:00 +0000

    Updated

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker + Jenkins + Nginx + Ngrok + Anvith  🚀"
+    return "Hello from Docker + Jenkins + Nginx + Ngrok + Anvith + Maggie  🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
